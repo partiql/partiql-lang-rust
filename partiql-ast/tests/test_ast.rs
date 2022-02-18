058eac30285c6bdec5a1e2d330ace25175d301b2
Arash M <27716912+am357@users.noreply.github.com>
2022-02-17 19:31:20 -0800

    Add Experimental AST
    This commit includes an experimental implementation of PartiQL AST
     based on the current kotlin implementation which has been
    auto-generated by `partiql-ir-generator`:
    - https://github.com/partiql/partiql-ir-generator
    
    We want to create an experimental PartiQL AST so that:
    
    - solidify our expected core model (at least for the first iteration)
      prior to developing AST code generation i.e. using
      partiql-ir-generator spec or a substitute.
    - we can get an early review with having integration with parser
      in the picture.
    
    This implementation does _not_ include:
    - The addition of MetaContainer and with_meta method.
      For code region data, We try to see if a substitute approach like
      using Span works as a substitute approach.
    
    - The implementation of std::fmt::Display Trait for ast entities.
      Will be tracked in a separate issue.
    - The tree traversal and transform utilities.
      Will be tracked in a separate issue.

---
+++ partiql-ast/tests/test_ast.rs
@@ -0,0 +1,22 @@
+mod common;
+
+use ion_rs::value::owned::OwnedValue;
+use ion_rs::value::AnyInt;
+use partiql_ast::experimental::ast::*;
+
+#[test]
+fn test_ast_init() {
+    common::setup();
+
+    let _i = Item {
+        kind: ItemKind::Query(Query {
+            expr: Expr {
+                kind: ExprKind::Lit(Lit {
+                    value: OwnedValue::Integer(AnyInt::I64(1)),
+                }),
+            },
+        }),
+    };
+
+    // TODO Add assertion once we have tree traversals
+}
