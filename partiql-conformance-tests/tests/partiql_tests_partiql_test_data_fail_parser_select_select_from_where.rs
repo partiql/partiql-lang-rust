5ef9bfe1d719f7aad5939efd7a395015091686ee
Alan Cai <caialan@amazon.com>
2022-04-05 01:10:54 -0700

    Add .gitignore and remove generated tests

--- partiql-conformance-tests/tests/partiql_tests_partiql_test_data_fail_parser_select_select_from_where.rs
+++
@@ -1,5 +0,0 @@
-#[test]
-fn sfw_without_where_expression_test() {
-    let parse_result = partiql_parser::lalr_parse("SELECT * FROM foo WHERE");
-    assert!(parse_result.is_err());
-}
--- partiql-conformance-tests/tests/partiql_tests_partiql_test_data_pass_parser_select_select_from_where.rs
+++
@@ -1,5 +0,0 @@
-#[test]
-fn select_with_single_from_test() {
-    let parse_result = partiql_parser::lalr_parse("SELECT a FROM table1");
-    assert!(parse_result.is_ok());
-}
