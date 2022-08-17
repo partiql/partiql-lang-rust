14c316e467f587315ecfac2c3756bc2b596ef0db
Almann Goo <almann@amazon.com>
2022-08-17 13:11:49 -0700

    build: compiles build scripts with optimization (#175)
    Without this running `lalrpop` runs for a long time unoptimized.  This
    trades off some build time to make the grammar generation much cheaper
    and makes the build 50% faster on my workstation.
    
    Also fixes a minor typo in `partiql-parser/build.rs`.

--- partiql-parser/build.rs
+++ partiql-parser/build.rs
@@ -4,9 +4,9 @@ use std::io;
 fn main() -> io::Result<()> {
     println!("cargo:rerun-if-changed=src/parse/partiql.lalrpop");
 
-    let grammer_dir = current_dir()?.join("src").join("parse");
+    let grammar_dir = current_dir()?.join("src").join("parse");
     lalrpop::Configuration::new()
-        .set_in_dir(grammer_dir)
+        .set_in_dir(grammar_dir)
         .process_current_dir()
         .expect("lalrpop process");
 
