a79c28ce2b795fd423b0f0d4848e1772b3d47909
Arash M <27716912+am357@users.noreply.github.com>
2022-11-01 21:08:02 -0700

    Add LoPlan

--- partiql-logical/src/lib.rs
+++ partiql-logical/src/lib.rs
@@ -6,9 +6,57 @@ use std::collections::HashMap;
 #[derive(Debug)]
 pub struct LogicalPlan(pub StableGraph<BindingsExpr, (), Directed>);
 
+#[derive(Debug)]
+pub struct OpId(usize);
+
+impl OpId {
+    fn index(&self) -> usize {
+        self.0
+    }
+}
+
 impl LogicalPlan {
     pub fn new() -> Self {
         LogicalPlan(StableGraph::<BindingsExpr, (), Directed>::new())
+    }
+}
+
+#[derive(Debug)]
+pub struct LgPlan<'a, T> {
+    nodes: Vec<T>,
+    edges: Vec<Vec<&'a OpId>>,
+    node_count: usize,
+}
+
+impl<'b, T> LgPlan<'b, T> {
+    fn new() -> Self {
+        LgPlan {
+            nodes: vec![],
+            edges: vec![],
+            node_count: 0,
+        }
+    }
+
+    fn add_operator(&mut self, op: T) -> OpId {
+        self.nodes.push(op);
+        self.edges.push(vec![]);
+        self.node_count += 1;
+        OpId(self.node_count)
+    }
+    fn add_relation<'a: 'b>(&mut self, src: &'a OpId, dst: &'a OpId) {
+        let src_index = src.index() - 1;
+        let dst_index = dst.index() - 1;
+        self.nodes.get(src_index).expect("No such operator exists");
+        self.nodes.get(dst_index).expect("No such operator exists");
+
+        // let mut edge:&mut Vec<&OpId> = self.edges.get(src_index).expect("some").borrow_mut();
+        self.edges[src_index].extend(vec![dst]);
+    }
+    fn operators(&self) -> &Vec<T> {
+        &self.nodes
+    }
+    fn relations(&self) -> &Vec<Vec<&OpId>> {
+        &self.edges
     }
 }
 
@@ -132,4 +180,22 @@ pub struct SelectValue {
 #[derive(Debug)]
 pub struct Distinct {
     pub out: Box<BindingsExpr>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_plan() {
+        let mut p:LgPlan<BindingsExpr> = LgPlan::new();
+        let a = p.add_operator(BindingsExpr::OrderBy);
+        let b = p.add_operator(BindingsExpr::Output);
+        let c = p.add_operator(BindingsExpr::Limit);
+        p.add_relation(&a, &b);
+        p.add_relation(&a, &c);
+        p.add_relation(&b, &c);
+        assert_eq!(3, p.operators().len());
+        assert_eq!(3, p.relations().len());
+    }
 }
