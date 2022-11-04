a3778d089f5b263a23d9af7fc3cf80e2b78d45bd
Josh Pschorr <josh@pschorr.dev>
2022-11-04 09:35:16 -0700

    Update Conformance Test Generator to latest partiql-tests (#208)
    Update Conformance Test Generator to latest partiql-tests

--- partiql-conformance-test-generator/src/reader.rs
+++ partiql-conformance-test-generator/src/reader.rs
@@ -1,219 +1,315 @@
-use ion_rs::value::owned::Element;
+use ion_rs::value::owned::{Element, Struct};
 use ion_rs::value::reader::{element_reader, ElementReader};
 use ion_rs::value::{IonElement, IonSequence, IonStruct};
 use ion_rs::IonType;
-use miette::IntoDiagnostic;
-use std::collections::HashSet;
+use miette::{miette, IntoDiagnostic};
 use std::ffi::OsStr;
 use std::fs;
 use std::fs::DirEntry;
 
-use crate::schema::spec::{
-    Assertion, Assertions, Namespace, Namespaces, TestCase, TestCases, TestDocument,
-};
-use crate::schema::structure::{TestDir, TestEntry, TestFile, TestRoot};
+use crate::schema::spec::*;
+use crate::schema::structure::*;
 use std::path::Path;
 
+macro_rules! expect_value {
+    ($val:expr, $msg:literal $(,)?) => {
+        match $val {
+            Some(v) => Ok(v),
+            None => Err(miette!($msg))
+        }?
+    };
+    ($val:expr, $fmt:expr, $($arg:tt)*) => {
+        match $val {
+            Some(v) => Ok(v),
+            None => Err(miette!($fmt, $($arg)*))
+        }?
+    };
+}
+macro_rules! expect_str {
+    ($val:expr, $msg:literal $(,)?) => {
+        expect_value!(
+            expect_value!($val, $msg).as_str(), "{} to be a string", $msg)
+    };
+    ($val:expr, $fmt:expr, $($arg:tt)*) => {
+        expect_value!(
+            expect_value!($val, $fmt, $($arg)*).as_str(),
+            "{} to be a string", format!($fmt, $($arg)*))
+
+    };
+}
+macro_rules! expect_struct {
+    ($val:expr, $msg:literal $(,)?) => {
+        expect_value!(
+            expect_value!($val, $msg).as_struct(), "{} to be a struct", $msg)
+    };
+    ($val:expr, $fmt:expr, $($arg:tt)*) => {
+        expect_value!(
+            expect_value!($val, $fmt, $($arg)*).as_struct(),
+            "{} to be a struct", format!($fmt, $($arg)*))
+
+    };
+}
+macro_rules! expect_list {
+    ($val:expr, $msg:literal $(,)?) => {
+        expect_value!(
+            expect_value!($val, $msg).as_sequence(), "{} to be a list", $msg)
+    };
+    ($val:expr, $fmt:expr, $($arg:tt)*) => {
+        expect_value!(
+            expect_value!($val, $fmt, $($arg)*).as_sequence(),
+            "{} to be a list", format!($fmt, $($arg)*))
+
+    };
+}
+
 pub fn read_schema(root: impl AsRef<Path>) -> miette::Result<TestRoot> {
-    read_root(&root).map(|r| TestRoot(r))
+    read_dir(&root).map(TestRoot)
 }
 
-fn read_root(root: impl AsRef<Path>) -> miette::Result<Vec<TestEntry>> {
-    let root = fs::read_dir(root)
-        .into_diagnostic()?
-        .filter_map(|entry| read_entry(entry.expect("entry")))
-        .collect();
-    Ok(root)
+fn read_dir(path: impl AsRef<Path>) -> miette::Result<Vec<TestEntry>> {
+    let mut entries = vec![];
+    for e in fs::read_dir(path).into_diagnostic()? {
+        if let Some(entry) = read_entry(e.into_diagnostic()?)? {
+            entries.push(entry);
+        }
+    }
+    Ok(entries)
 }
 
-fn read_entry(entry: DirEntry) -> Option<TestEntry> {
-    let file_type = entry.file_type().expect("file_type");
-    let name = entry.file_name().into_string().expect("file name");
+fn read_entry(entry: DirEntry) -> miette::Result<Option<TestEntry>> {
+    let file_type = entry.file_type().into_diagnostic()?;
+    let name = entry.file_name().into_string().unwrap();
 
     let ion_file_extension = OsStr::new("ion");
-    if file_type.is_file() {
+    let result = if file_type.is_file() {
         if entry.path().extension() == Some(ion_file_extension) {
-            let contents = read_test_doc(entry);
             Some(TestEntry::Doc(TestFile {
                 file_name: name,
-                contents,
+                contents: read_test_doc(entry.path())?,
             }))
         } else {
             None
         }
     } else if file_type.is_dir() {
-        let contents = fs::read_dir(entry.path())
-            .expect("read_dir")
-            .into_iter()
-            .filter_map(|x| read_entry(x.expect("entry")))
-            .collect();
         Some(TestEntry::Dir(TestDir {
             dir_name: name,
-            contents,
+            contents: read_dir(entry.path())?,
         }))
     } else {
         unreachable!()
-    }
-}
-
-fn read_test_doc(entry: DirEntry) -> TestDocument {
-    let buf = fs::read(entry.path()).unwrap();
-    let elements = element_reader().read_all(&buf).unwrap();
-    ion_data_to_test_document(elements)
-}
-
-/// Converts a vector of Ion data into a `TestDocument`, which can be composed of `Namespace`s
-/// and `TestCase`s. `Namespace`s must be provided as IonLists while `TestCase`s must be provided
-/// as IonStructs. Other Ion types will result in a panic.
-///
-/// When encountering a duplicate namespace/test case, a namespace/test case will be added with
-/// '_0' suffixed to the end of the name.
-fn ion_data_to_test_document(all_ion_data: Vec<Element>) -> TestDocument {
-    let mut namespaces = Vec::new();
-    let mut test_cases = Vec::new();
-
-    let mut encountered_ns_names: HashSet<String> = HashSet::new();
-    let mut encountered_tc_names: HashSet<String> = HashSet::new();
-
-    for elem in all_ion_data {
-        match elem.ion_type() {
-            IonType::List => {
-                // namespace in document
-                let mut ns = test_namespace(&elem);
-                if encountered_ns_names.contains(&ns.name) {
-                    ns.name.push_str("_0")
-                }
-                encountered_ns_names.insert(ns.name.clone());
-                namespaces.push(ns)
-            }
-            IonType::Struct => {
-                // test case in document
-                let mut tc = test_case(&elem);
-                if encountered_tc_names.contains(&tc.test_name) {
-                    tc.test_name.push_str("_0")
-                }
-                encountered_tc_names.insert(tc.test_name.clone());
-                test_cases.push(tc)
-            }
-            _ => panic!("Document parsing requires an IonList or IonStruct"),
-        }
-    }
-    TestDocument {
-        namespaces,
-        test_cases,
-    }
-}
-
-/// Parses the given `OwnedElement` to a `Namespace`. Requires an annotation to provided that will
-/// be used to create the name. '_namespace' will be suffixed to the first annotation provided.
-/// The namespace can contain sub-namespaces and test cases represented by IonLists and IonStructs
-/// respectively. When provided with something other than an IonList or IonStruct, this function
-/// will panic.
-fn test_namespace(element: &Element) -> Namespace {
-    let annot: Vec<_> = element
-        .annotations()
-        .map(|a| a.text().expect("annotation text"))
-        .collect();
-    let name = annot
-        .first()
-        .expect("expected an annotation for the namespace")
-        .to_string();
-
-    let mut namespaces: Namespaces = Vec::new();
-    let mut test_cases: TestCases = Vec::new();
-
-    let mut encountered_ns_names: HashSet<String> = HashSet::new();
-    let mut encountered_tc_names: HashSet<String> = HashSet::new();
-
-    for ns_or_test in element.as_sequence().expect("namespace is list").iter() {
-        match ns_or_test.ion_type() {
-            // namespace within the namespace
-            IonType::List => {
-                let mut ns = test_namespace(ns_or_test);
-                if encountered_ns_names.contains(&ns.name) {
-                    ns.name.push_str("_0")
-                }
-                encountered_ns_names.insert(ns.name.clone());
-                namespaces.push(ns)
-            }
-            // test case within the namespace
-            IonType::Struct => {
-                let mut tc = test_case(ns_or_test);
-                if encountered_tc_names.contains(&tc.test_name) {
-                    tc.test_name.push_str("_0")
-                }
-                encountered_tc_names.insert(tc.test_name.clone());
-                test_cases.push(tc)
-            }
-            _ => panic!("Namespace parsing requires an IonList or IonStruct"),
-        }
-    }
-    Namespace {
-        name,
-        namespaces,
-        test_cases,
-    }
-}
-
-/// Parses the given IonStruct to a `TestCase`. The IonStruct requires two string fields with the
-/// 'name' and 'statement' in addition to an 'assert' field containing one or more `Assertions`.
-///
-/// For test assertions that are not supported (e.g. StaticAnalysisFail), the assertion of
-/// `NotYetImplemented` will be used.
-fn test_case(element: &Element) -> TestCase {
-    let test_struct = element.as_struct().expect("struct");
-    let test_name = test_struct
-        .get("name")
-        .expect("name")
-        .as_str()
-        .expect("as_str()")
-        .to_string();
-    let statement = test_struct
-        .get("statement")
-        .expect("statement")
-        .as_str()
-        .expect("as_str()")
-        .to_string();
-
-    let assert_field = test_struct.get("assert").expect("assert field missing");
-    let assertions_vec: Vec<_> = match assert_field.ion_type() {
-        IonType::Struct => vec![assert_field],
-        IonType::List => assert_field
-            .as_sequence()
-            .expect("as_sequence")
-            .iter()
-            .collect(),
-        _ => panic!("Invalid IonType for the test case assertions"),
     };
-    let assertions = assertions(&assertions_vec);
-    TestCase {
-        test_name,
-        statement,
-        assertions,
+
+    Ok(result)
+}
+
+fn read_test_doc(path: impl AsRef<Path>) -> miette::Result<PartiQLTestDocument> {
+    let buf = fs::read(path).into_diagnostic()?;
+    buf.as_slice().try_into()
+}
+
+impl TryFrom<&[u8]> for PartiQLTestDocument {
+    type Error = miette::Report;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let elements = element_reader().read_all(value).into_diagnostic()?;
+        elements.as_slice().try_into()
     }
 }
 
-/// Converts the vector of Ion values into `Assertions`. Checks that a result field is provided
-/// in the vector and has the symbol 'SyntaxSuccess' or 'SyntaxFail', which correspond to
-/// `Assertion::SyntaxSuccess` and `Assertion::SyntaxFail` respectively. Other assertion symbols
-/// will default to `Assertion::NotYetImplemented`
-fn assertions(assertions: &Vec<&Element>) -> Assertions {
-    let mut test_case_assertions: Assertions = Vec::new();
-    for assertion in assertions {
-        let assertion_struct = assertion.as_struct().expect("as_struct()");
-        let parse_result = assertion_struct.get("result");
+impl TryFrom<&[Element]> for PartiQLTestDocument {
+    type Error = miette::Report;
 
-        if let Some(r) = parse_result {
-            let r_as_str = r.as_str().expect("as_str()");
-            let assertion = match r_as_str {
-                "SyntaxSuccess" => Assertion::SyntaxSuccess,
-                "SyntaxFail" => Assertion::SyntaxFail,
-                _ => Assertion::NotYetImplemented,
-            };
-            test_case_assertions.push(assertion);
+    fn try_from(value: &[Element]) -> Result<Self, Self::Error> {
+        let inner: Result<Vec<_>, _> = value.iter().map(|e| e.try_into()).collect();
+        Ok(PartiQLTestDocument(inner?))
+    }
+}
+
+impl TryFrom<&Element> for TestVariant {
+    type Error = miette::Report;
+
+    fn try_from(value: &Element) -> Result<Self, Self::Error> {
+        let typ = value.ion_type();
+        if value.is_null() {
+            Err(miette!("value was null when parsing TestVariant"))
+        } else {
+            match typ {
+                IonType::List => {
+                    let contents = value.try_into()?;
+                    Ok(TestVariant::Namespace(contents))
+                }
+                IonType::Struct => {
+                    let strct = value.as_struct().unwrap();
+                    if value.has_annotation("envs") {
+                        Ok(TestVariant::Environments(strct.try_into()?))
+                    } else if value.has_annotation("equiv_class") {
+                        Ok(TestVariant::EquivalenceClass(strct.try_into()?))
+                    } else {
+                        Ok(TestVariant::TestCase(strct.try_into()?))
+                    }
+                }
+                _ => Err(miette!(
+                    "unexpected type {:?} when parsing TestVariant",
+                    typ
+                )),
+            }
         }
     }
-    test_case_assertions
+}
+
+impl TryFrom<&Element> for Namespace {
+    type Error = miette::Report;
+
+    fn try_from(value: &Element) -> Result<Self, Self::Error> {
+        let name = if let Some(sym) = value.annotations().next() {
+            sym.text().unwrap().to_string()
+        } else {
+            return Err(miette!(
+                "expected annotation for name when parsing Namespace"
+            ));
+        };
+        let list = expect_list!(Some(value), "Namespace");
+        let contents: Result<Vec<_>, _> = list.iter().map(|e| e.try_into()).collect();
+        Ok(Namespace {
+            name,
+            contents: contents?,
+        })
+    }
+}
+
+impl TryFrom<&Struct> for TestCase {
+    type Error = miette::Report;
+
+    fn try_from(value: &Struct) -> Result<Self, Self::Error> {
+        let name = expect_str!(value.get("name"), "TestCase name").into();
+        let statement = expect_str!(value.get("statement"), "TestCase statement").into();
+        let env = if let Some(v) = value.get("env") {
+            Some(expect_struct!(Some(v), "TestCase envs").clone())
+        } else {
+            None
+        };
+        let assert_elt = expect_value!(value.get("assert"), "TestCase assert");
+        let assert = match assert_elt.ion_type() {
+            IonType::List => {
+                let list = assert_elt.as_sequence().unwrap();
+                let asserts: Result<Vec<_>, _> = list.iter().map(|e| e.try_into()).collect();
+                asserts?
+            }
+            IonType::Struct => {
+                vec![assert_elt.try_into()?]
+            }
+            _ => {
+                return Err(miette!(
+                    "unexpected type {:?} when parsing TestCase assert",
+                    assert_elt.ion_type()
+                ))
+            }
+        };
+
+        Ok(TestCase {
+            name,
+            statement,
+            env,
+            assert,
+        })
+    }
+}
+
+impl TryFrom<&Element> for Assertion {
+    type Error = miette::Report;
+
+    fn try_from(value: &Element) -> Result<Self, Self::Error> {
+        let val_struct = expect_struct!(Some(value), "Assertion");
+        let result = expect_str!(val_struct.get("result"), "Assertion result field").to_string();
+
+        let assertion = match result.as_str() {
+            "SyntaxSuccess" => SyntaxSuccessAssertion { result }.into(),
+            "SyntaxFail" => SyntaxFailAssertion { result }.into(),
+            "StaticAnalysisFail" => StaticAnalysisFailAssertion { result }.into(),
+            "EvaluationSuccess" => {
+                let eval_mode = expect_value!(
+                    val_struct.get("evalMode"),
+                    "EvaluationSuccessAssertion evalMode"
+                )
+                .try_into()?;
+                let output = expect_value!(
+                    val_struct.get("output"),
+                    "EvaluationSuccessAssertion output"
+                )
+                .clone();
+                EvaluationSuccessAssertion {
+                    result,
+                    output,
+                    eval_mode,
+                }
+                .into()
+            }
+            "EvaluationFail" => {
+                let eval_mode = expect_value!(
+                    val_struct.get("evalMode"),
+                    "EvaluationFailAssertion evalMode"
+                )
+                .try_into()?;
+                EvaluationFailAssertion { result, eval_mode }.into()
+            }
+            _ => return Err(miette!("unknown assertion type {:?}", result)),
+        };
+        Ok(assertion)
+    }
+}
+
+impl TryFrom<&Element> for EvaluationModeSymbolOrList {
+    type Error = miette::Report;
+
+    fn try_from(value: &Element) -> Result<Self, Self::Error> {
+        let eval_mode = |value: &Element| {
+            let eval_mode = expect_str!(Some(value), "Eval Mode");
+            match eval_mode {
+                "EvalModeError" => Ok(EvaluationMode::EvalModeError),
+                "EvalModeCoerce" => Ok(EvaluationMode::EvalModeCoerce),
+                _ => Err(miette!("unknown Eval Mode type {:?}", eval_mode)),
+            }
+        };
+
+        match value.ion_type() {
+            IonType::Symbol | IonType::String => Ok(eval_mode(value)?.into()),
+            IonType::List => {
+                let list = value.as_sequence().unwrap();
+                let eval_modes: Result<Vec<_>, _> = list.iter().map(|e| eval_mode(e)).collect();
+                Ok(eval_modes?.into())
+            }
+            _ => Err(miette!(
+                "unexpected type {:?} when parsing EvaluationModeSymbolOrList",
+                value.ion_type()
+            )),
+        }
+    }
+}
+
+impl TryFrom<&Struct> for Environments {
+    type Error = miette::Report;
+
+    fn try_from(value: &Struct) -> Result<Self, Self::Error> {
+        Ok(Environments {
+            envs: value.clone(),
+        })
+    }
+}
+
+impl TryFrom<&Struct> for EquivalenceClass {
+    type Error = miette::Report;
+
+    fn try_from(value: &Struct) -> Result<Self, Self::Error> {
+        let id = expect_str!(value.get("id"), "EquivalenceClass id").into();
+        let stmt_list = expect_list!(value.get("statements"), "EquivalenceClass statements");
+
+        let mut statements = vec![];
+        for stmt in stmt_list.iter() {
+            statements.push(expect_str!(Some(stmt), "EquivalenceClass statement").into())
+        }
+
+        Ok(EquivalenceClass { id, statements })
+    }
 }
 
 #[cfg(test)]
