131febf2d1c9707a4acd406b59fed13a94285bf3
Alan Cai <caialan@amazon.com>
2022-12-02 16:41:54 -0500

    Implements `IS` (unknown types only), `NULLIF`, and `COALESCE` (#229)
    * Add IS (NOT) NULL/MISSING implementation
    
    * Add NULLIF and COALESCE implementations using CASE WHEN expressions

--- partiql-logical/src/lib.rs
+++ partiql-logical/src/lib.rs
@@ -109,6 +109,55 @@ pub enum PathComponent {
 }
 
 #[derive(Clone, Debug)]
+pub struct IsTypeExpr {
+    pub not: bool,
+    pub expr: Box<ValueExpr>,
+    pub is_type: Type,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Type {
+    NullType,
+    BooleanType,
+    Integer2Type,
+    Integer4Type,
+    Integer8Type,
+    DecimalType,
+    NumericType,
+    RealType,
+    DoublePrecisionType,
+    TimestampType,
+    CharacterType,
+    CharacterVaryingType,
+    MissingType,
+    StringType,
+    SymbolType,
+    BlobType,
+    ClobType,
+    DateType,
+    TimeType,
+    ZonedTimestampType,
+    StructType,
+    TupleType,
+    ListType,
+    SexpType,
+    BagType,
+    AnyType,
+    // TODO CustomType
+}
+
+#[derive(Clone, Debug)]
+pub struct NullIfExpr {
+    pub lhs: Box<ValueExpr>,
+    pub rhs: Box<ValueExpr>,
+}
+
+#[derive(Clone, Debug)]
+pub struct CoalesceExpr {
+    pub elements: Vec<ValueExpr>,
+}
+
+#[derive(Clone, Debug)]
 #[allow(dead_code)] // TODO remove once out of PoC
 pub enum ValueExpr {
     // TODO other variants
@@ -123,6 +172,9 @@ pub enum ValueExpr {
     BetweenExpr(BetweenExpr),
     SimpleCase(SimpleCase),
     SearchedCase(SearchedCase),
+    IsTypeExpr(IsTypeExpr),
+    NullIfExpr(NullIfExpr),
+    CoalesceExpr(CoalesceExpr),
 }
 
 #[derive(Clone, Debug, Default)]
