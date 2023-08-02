738c928d96dc782d05cad0e588569e25d09fa2bb
Josh Pschorr <joshps@amazon.com>
2023-08-02 14:01:33 -0700

    Clippy

--- partiql-eval/src/eval/expr/operators.rs
+++ partiql-eval/src/eval/expr/operators.rs
@@ -8,8 +8,7 @@ use crate::eval::expr::{BindError, BindEvalExpr, EvalExpr};
 use crate::eval::EvalContext;
 
 use partiql_types::{
-    ArrayType, BagType, PartiqlType, StructType, TypeKind, TYPE_ANY, TYPE_BOOL,
-    TYPE_NUMERIC_TYPES,
+    ArrayType, BagType, PartiqlType, StructType, TypeKind, TYPE_ANY, TYPE_BOOL, TYPE_NUMERIC_TYPES,
 };
 use partiql_value::Value::{Boolean, Missing, Null};
 use partiql_value::{BinaryAnd, BinaryOr, NullableEq, NullableOrd, Value};
