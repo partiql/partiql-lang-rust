85d08fd162487d5c3af0d8a209bde4f59e2b59d6
Arash M <27716912+am357@users.noreply.github.com>
2023-08-28 15:11:23 -0700

    Add Catalog to resolve_varref in lowering

--- partiql-logical-planner/src/lower.rs
+++ partiql-logical-planner/src/lower.rs
@@ -361,6 +361,18 @@ impl<'a> AstToLogical<'a> {
                                                     lookups.push(expr);
                                                 }
                                                 continue;
+                                            } else if let Some(_type_entry) = self
+                                                .catalog
+                                                .resolve_type(name_ref.sym.value.as_ref())
+                                            {
+                                                let expr = ValueExpr::VarRef(
+                                                    var_binding.clone(),
+                                                    VarRefType::Global,
+                                                );
+                                                if !lookups.contains(&expr) {
+                                                    lookups.push(expr);
+                                                }
+                                                continue;
                                             } else {
                                                 let path = logical::ValueExpr::Path(
                                                     Box::new(ValueExpr::VarRef(
