970394bb531af8543958b422104b4434f65e84a4
Josh Pschorr <joshps@amazon.com>
2025-01-09 11:05:27 -0800

    Cleanup Boxed Variant

--- partiql-logical-planner/src/typer.rs
+++ partiql-logical-planner/src/typer.rs
@@ -346,7 +346,7 @@ impl<'c> PlanTyper<'c> {
                     Lit::Double(_) => type_float64!(self.bld),
                     Lit::Bool(_) => type_bool!(self.bld),
                     Lit::String(_) => type_string!(self.bld),
-                    Lit::BoxDocument(_, _) => type_dynamic!(self.bld), // TODO
+                    Lit::Variant(_, _) => type_dynamic!(self.bld), // TODO
                     Lit::Struct(_) => type_struct!(self.bld),
                     Lit::Bag(_) => type_bag!(self.bld),
                     Lit::List(_) => type_array!(self.bld),
